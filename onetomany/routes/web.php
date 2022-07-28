703b305703075d1aa0dc4a1c3fe92fe57a6bcd8e
IgorMakhinich <igor.makhinich@gmail.com>
2022-07-28 22:46:59 +0200

    Eloquent One to Many Relationship CRUD

--- onetomany/routes/web.php
+++ onetomany/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create/{id}', function ($id) {
+    $user = User::findOrFail($id);
+
+    $post = new Post(['title' => 'New Post Title3', 'body' => 'New Body Title3']);
+
+    $user->posts()->save($post);
+});
+
+Route::get('read/{id}', function ($id) {
+    $user = User::findOrFail($id);
+
+    // dd($user->posts);
+
+    foreach ($user->posts as $post) {
+        echo $post->title . "<br>";
+    }
+});
+
+Route::get('update/{id}/{post_id}', function ($id, $post_id) {
+    $user = User::findOrFail($id);
+
+    // dd($user->posts);
+    // return $user->posts()->whereId($post_id)->update(['title'=>'Title ' . $post_id . 'has updated']);
+    return $user->posts()->where('id','=',$post_id)->update(['title'=>'Title ' . $post_id . 'has updated']);
+});
+
+Route::get('delete/{user_id}/{post_id}', function($user_id, $post_id) {
+    $user = User::findOrFail($user_id);
+
+    return $user->posts()->whereId($post_id)->delete();
 });
